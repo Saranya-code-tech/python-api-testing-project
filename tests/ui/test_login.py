955a9b808e599c6aecc8657aaf5346eadbd75393
Saranya Ethiraj <ethiraj.saranya@gmail.com>
2026-05-01 23:52:34 -0500

    Refactored tests to use JSON-based test data (data-driven testing)

--- tests/ui/test_login.py
+++ tests/ui/test_login.py
@@ -1,17 +1,23 @@
 from pages.login_page import LoginPage
+from utils.config import load_test_data
 
 def test_valid_login(driver):
-    login_page = LoginPage(driver)
+    data = load_test_data()
+    user = data["valid_user"]
 
+    login_page = LoginPage(driver)
     login_page.load()
-    login_page.login("standard_user", "secret_sauce")
+    login_page.login(user["username"], user["password"])
 
     assert "inventory" in driver.current_url
 
-def test_invalid_login(driver):
-    login_page = LoginPage(driver)
 
+def test_invalid_login(driver):
+    data = load_test_data()
+    user = data["invalid_user"]
+
+    login_page = LoginPage(driver)
     login_page.load()
-    login_page.login("locked_out_user", "secret_sauce")
+    login_page.login(user["username"], user["password"])
 
     assert "Sorry, this user has been locked out" in login_page.get_error_message()
--- utils/config.py
+++ utils/config.py
@@ -1,1 +1,10 @@
+import json
+import os
+
 base_url = "https://jsonplaceholder.typicode.com"
+
+def load_test_data():
+    file_path = os.path.join(os.path.dirname(__file__), "../test_data/test_data.json")
+
+    with open(file_path, "r") as file:
+        return json.load(file)
