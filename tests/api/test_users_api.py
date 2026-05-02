b3639daab3362d2604ec378aff4b0aacc28079f1
Saranya Ethiraj <ethiraj.saranya@gmail.com>
2026-05-01 23:24:46 -0500

    Added UI test structure with login test

--- tests/api/test_users_api.py
+++ tests/api/test_users_api.py
@@ -1,5 +1,9 @@
 import pytest
+import requests
+
 from utils.api_client import get_request
+from utils.config import base_url
+
 
 @pytest.fixture
 def users_response():
@@ -40,3 +44,29 @@ def test_first_user(users_response):
     assert isinstance(first_user["address"], dict)
     assert "city" in first_user["address"]
     assert first_user["address"]["city"] is not None
+
+@pytest.mark.parametrize("user_id", [1, 2, 3])
+def test_get_user_by_id(user_id):
+    response = get_request(f"/users/{user_id}")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["id"] == user_id
+
+def test_schema_validation():
+    response = get_request(f"/users/1")
+    data = response.json()
+    assert response.status_code == 200
+    assert isinstance(data["name"], str)
+    assert isinstance(data["email"], str)
+    assert isinstance(data["id"], int)
+    assert isinstance(data["address"], dict)
+
+def test_query_parameter():
+    response = requests.get(base_url + "/posts", params={"userId": 1})
+    data = response.json()
+    assert response.status_code == 200
+    assert isinstance(data, list)
+    assert len(data) > 0
+    for post in data:
+        assert post["userId"] == 1
+
