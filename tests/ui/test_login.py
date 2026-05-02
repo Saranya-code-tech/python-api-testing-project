98f187f63f38ab1169d4072b235148a0e7d6125c
Saranya Ethiraj <ethiraj.saranya@gmail.com>
2026-05-02 00:06:52 -0500

    Added combined UI and API validation test

--- tests/ui/test_login.py
+++ tests/ui/test_login.py
@@ -1,5 +1,6 @@
 from pages.login_page import LoginPage
 from utils.config import load_test_data
+import requests
 
 def test_valid_login(driver):
     data = load_test_data()
@@ -21,3 +22,18 @@ def test_invalid_login(driver):
     login_page.login(user["username"], user["password"])
 
     assert "Sorry, this user has been locked out" in login_page.get_error_message()
+
+def test_login_and_validate_api_response(driver):
+    login_page = LoginPage(driver)
+
+    login_page.load()
+    login_page.login("standard_user", "secret_sauce")
+
+    assert "inventory" in driver.current_url
+
+    response = requests.get("https://jsonplaceholder.typicode.com/users/1")
+    data = response.json()
+
+    assert response.status_code == 200
+    assert data["id"] == 1
+    assert data["email"] == "Sincere@april.biz"
